acf99b34f6d576e17ce8cbe2aa71e9b2ca2f49a2
rhernandezmy <rhernandezmy@fpvirtualaragon.es>
2025-12-12 17:28:16 +0100

    Vista funcionando

--- tareas_dwes/tareas/views.py
+++ tareas_dwes/tareas/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import DetailView
+from .models import Tarea
+
+class DetalleTarea(DetailView):
+
+    model = Tarea
+    template_name = 'tareas/detalle_tarea.html'
+    context_object_name = 'tarea'
